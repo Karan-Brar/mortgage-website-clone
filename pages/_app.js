346067dd1f67ec696975d14a4f815c2b2dae6fd4
tadghh <thenry.he@gmail.com>
2023-07-01 18:45:51 -0500

    Header SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "@/styles/globals.css";
 import NavigationBar from "@/components/NavigationBar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>Sky Mortgages</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Mortgage company text"
+        />
+      </Head>
       <NavigationBar />
       <Component {...pageProps} classNames={"opacity - 1 "} />
     </div>
