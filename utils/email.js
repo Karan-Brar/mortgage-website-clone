060f3c26bd266cda5174d271d0b5d42f1083d1ec
tadghh <thenry.he@gmail.com>
2023-07-10 02:33:44 -0500

    email update

--- utils/email.js
+++ utils/email.js
@@ -2,49 +2,49 @@ const sgMail = require("@sendgrid/mail");
 import Handlebars from "handlebars";
 import fs from "fs";
 
-export const sendContactEmail = inquiryInfo => {
-	//somehow load below template
-	const emailTemplate = Handlebars.compile(
-		fs.readFileSync(process.cwd() + "/emails/contact-email.html", "utf-8")
-	);
+export const sendContactEmail = (inquiryInfo) => {
+  //somehow load below template
+  const emailTemplate = Handlebars.compile(
+    fs.readFileSync(process.cwd() + "/emails/contact-email.html", "utf-8")
+  );
 
-	sgMail.setApiKey(process.env.SEND_GRID_KEY);
+  sgMail.setApiKey(process.env.SEND_GRID_KEY);
 
-	const {
-		custRequest,
-		clientEmail,
-		clientPhoneNumber,
-		data,
-		custGoal,
-		custDownPayment,
-		buyingPlan,
-		mortgageEnd,
-		fullName,
-	} = inquiryInfo;
+  const {
+    custRequest,
+    clientEmail,
+    clientPhoneNumber,
+    data,
+    custGoal,
+    custDownPayment,
+    buyingPlan,
+    mortgageEnd,
+    fullName,
+  } = inquiryInfo;
 
-	const msg = {
-		to: "thenry.he@gmail.com", // Change to your recipient
-		from: process.env.SEND_GRID_EMAIL, // Change to your verified sender
-		subject: "Test Client email",
+  const msg = {
+    to: clientEmail, // Change to your recipient
+    from: process.env.SEND_GRID_EMAIL, // Change to your verified sender
+    subject: "Enquiry: " + clientEmail,
 
-		html: emailTemplate({
-			custRequest,
-			clientEmail,
-			clientPhoneNumber,
-			data,
-			custGoal,
-			custDownPayment,
-			buyingPlan,
-			mortgageEnd,
-			fullName,
-		}),
-	};
-	sgMail
-		.send(msg)
-		.then(() => {
-			console.log("Email sent!");
-		})
-		.catch(error => {
-			console.error(error);
-		});
+    html: emailTemplate({
+      custRequest,
+      clientEmail,
+      clientPhoneNumber,
+      data,
+      custGoal,
+      custDownPayment,
+      buyingPlan,
+      mortgageEnd,
+      fullName,
+    }),
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent!");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
