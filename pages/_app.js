ad5b87e3e1669540819d033df78fb650539e17d1
Karanvir Brar <brarkaran938@gmail.com>
2023-07-21 17:43:22 -0500

    SEO Stuff

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,23 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Mortgage Fox</title>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="robots" content="all" />
         <meta
           name="description"
-          content="Customized Mortgage solutions"
+          content="Mortgage Fox is a mortgage company based in Winnipeg, specializing in providing tailored mortgage solutions to meet the unique needs of every client. With a team of experienced professionals, we are committed to helping individuals and families achieve their homeownership dreams. "
         />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/assets/images/mortgage-fox-logo-resized.png"
+        />
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta property="og:url" content="https://www.mortgagefox.ca/"></meta>
+        <meta
+          property="og:image"
+          content="/assets/images/mortgage-fox-thumbnail.png"
+        ></meta>
       </Head>
       <NavigationBar />
       <Component {...pageProps} classNames={"opacity - 1 "} />
